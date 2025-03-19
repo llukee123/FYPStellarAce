26045460d5fa5f3e4c201bf0a4c3933d4e9baa77
llukee123 <lukecraven2004@gmail.com>
2025-03-19 08:10:57 +0000

    collision avoidance

--- Source/FYPStellarAce/FYPStellarAce.Build.cs
+++ Source/FYPStellarAce/FYPStellarAce.Build.cs
@@ -1,5 +1,6 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
+using System.IO;
 using UnrealBuildTool;
 
 public class FYPStellarAce : ModuleRules
@@ -9,15 +10,5 @@ public class FYPStellarAce : ModuleRules
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 	
 		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore" });
-
-		PrivateDependencyModuleNames.AddRange(new string[] {  });
-
-		// Uncomment if you are using Slate UI
-		// PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
-		
-		// Uncomment if you are using online features
-		// PrivateDependencyModuleNames.Add("OnlineSubsystem");
-
-		// To include OnlineSubsystemSteam, add it to the plugins section in your uproject file with the Enabled attribute set to true
-	}
+    }
 }
